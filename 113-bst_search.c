939cfb051586ee8176caafd0ecb0014eace12df1
KHADRAOUI HAMZA <138809190+Bbr011@users.noreply.github.com>
2024-09-19 15:06:22 +0100

    bst_search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -4,7 +4,6 @@
  * bst_search - Searches for a value in a binary search tree.
  * @tree: A pointer to the root node of the BST to search.
  * @value: The value to search for in the BST.
- *
  * Return: If the tree is NULL or the value is not found, NULL.
  *         Otherwise, a pointer to the node containing the value.
  */
